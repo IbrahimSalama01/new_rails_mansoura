d05130b02a1046cb528dda7fdd591a57f6bebd62
Ibrahim_Salama <ibrahimsalama100@outlook.com>
2025-05-10 22:57:15 +0300

    Refactor UsersController to update user creation and update logic, removing unnecessary fields and improving formatting.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,57 @@
 class UsersController < ApplicationController
 
     def index 
-        @users = User.all
+      @users = User.all
     end
-
+  
     def show 
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
-
+  
     def new
-      redirect_to @user, notice: "User was successfully created."
+      @user = User.new
     end
-
+  
     def create
-        user = User.new(name:params[:user][:name] age:params[:user][:age] track: params[:user][:track])
-        if user.save
-            redirect_to user
-        else
-            render :new, status: :unprocessable_entity
-        end
-
+      @user = User.new(
+        name: params[:user][:name],
+        dob: params[:user][:dob],
+        email: params[:user][:email],
+        phone_number: params[:user][:phone_number],
+        address: params[:user][:address]
+      )
+  
+      if @user.save
+        redirect_to @user, notice: "User was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
-    
+  
     def edit 
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
-
+  
     def update
-        @user = User.find(params[:id])
-        @user.name = params[:user][:name]
-        @user.age = params[:user][:age]
-        @user.track = params[:user][:track]
-        if @user.()
-        if @user.save
-            redirect_to @user, notice: "user updated succesfully"
-        else
-            render :edit, status: :unprocessable_entity
-        end
+      @user = User.find(params[:id])
+      @user.name = params[:user][:name]
+      @user.dob = params[:user][:dob]
+      @user.email = params[:user][:email]
+      @user.phone_number = params[:user][:phone_number]
+      @user.address = params[:user][:address]
+  
+      if @user.save
+        redirect_to @user, notice: "User was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
-
+  
     def destroy
-        @user = User.find(params[:id])
-        @user.destroy 
-        redirect_to users_path, notice: "User was successfully deleted."
+      @user = User.find(params[:id])
+      @user.destroy 
+      redirect_to users_path, notice: "User was successfully deleted."
     end
-        
-end
+  
+  end
+  
